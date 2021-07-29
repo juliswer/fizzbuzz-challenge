fb3acfda31d43ad6e9d4f2faa4237055c4affd2d
Julián Swerdlin <julianswer36@gmail.com>
2021-07-28 21:47:27 -0300

    new files

---
+++ fizzbuzz.js
@@ -0,0 +1,30 @@
+function fizzbuzz(num) {
+    if (typeof num !== 'number') {
+        console.log('Error: the argument must be a number');
+    }
+    if(num === 0) {
+        return null;
+    }
+    if(num % 3 === 0 && num % 5 === 0) {
+        return 'fizzbuzz'
+    }
+
+    if(num % 3 === 0) {
+        return 'fizz'
+    }
+
+    if(num % 5 === 0) {
+        return 'buzz'
+    }
+    return num
+}
+
+function print(num) {
+    for(var i = 0; i <= num; i++) {
+        console.log(`${i}: ${fizzbuzz(i)}`);
+    }
+}
+
+print(16)
+
+module.exports = fizzbuzz;
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,5 +1,33 @@
+const fizzbuzz = require('./fizzbuzz');
+
 describe('fizzbuzz', () => {
-    test('test', () => {
-        expect(true).toBe(true);
+    test('should print 1 if they receive 1', () => {
+        const expected = 1;
+        const result = fizzbuzz(1);
+        expect(expected).toBe(result);
+    });
+
+    test('should print fizz if they receive 3', () => {
+        const expected = 'fizz';
+        const result = fizzbazz(3);
+        expect(expected).toBe(result)
+    })
+
+    test('should print fizz if they receive a multiple of 3', () => {
+        const expected = 'fizz'
+        const result = fizzbazz(6);
+        expect(expected).toBe(result)
+    })
+
+    test("should print buzz if they receive a multiple of 5", () => {
+        const expected = 'buzz';
+        const result = fizzbazz(10);
+        expect(expected).toBe(result);
+    })
+
+    test("should print fizzbuzz if they receive a multiple of 3 and 5", () => {
+        const expected = 'fizzbuzz';
+        const result = fizzbuzz(15);
+        expect(expected).toBe(result);
     })
 })
